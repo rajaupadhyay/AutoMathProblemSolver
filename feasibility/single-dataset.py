fb2379437c0166900cad6ec41689348420879f92
jasko1212si <jas.semrl@gmail.com>
2019-02-17 16:18:16 +0000

    more feasibility stuff

--- feasibility/single-dataset.py
+++ feasibility/single-dataset.py
@@ -1,4 +1,5 @@
 import json
+from math import *
 
 newData = []
 
@@ -10,6 +11,11 @@ raw2 = json.load(fp2)
 raw3 = json.load(fp3)
 
 count = 0
+nonNum = 0
+wrongSolution = 0
+inconsistentUnknowns = 0
+emptyQuestion = 0
+emptyUnknowns = 0
 mcq = 0
 unsure = 0
 
@@ -37,6 +43,7 @@ for entry in raw1+raw2:
     try:
         answers = [[float(eval(o)) for o in option] for option in answers]
     except:
+        nonNum += 1
         if answers[0][0] in ['(A)', '(B)', '(C)', '(D)', '(E)']:
             mcq += 1
         bad = True
@@ -46,12 +53,14 @@ for entry in raw1+raw2:
     if not bad:
         for u in unknowns:
             if len(u) != 1:
+                emptyUnknowns +=1
                 bad = True
                 break
 
     if not bad:
         for option in answers:
             if len(option) != len(unknowns):
+                inconsistentUnknowns += 1
                 bad = True
                 break
 
@@ -60,15 +69,28 @@ for entry in raw1+raw2:
         for option in answers:
             for e in equations:
                 temp = '('+e.replace('=', ')-(').replace('^', '**')+')'
+                charset1a = unknowns + [')']
+                charset2b = unknowns + ['(']
+                charset2a = charset1a + [str(i) for i in range(10)]
+                charset1b = charset2b + [str(i) for i in range(10)]
+                for a in charset1a:
+                    for b in charset1b:
+                        temp = temp.replace(a+b, a+'*'+b)
+                for a in charset2a:
+                    for b in charset2b:
+                        temp = temp.replace(a+b, a+'*'+b)
                 for i in range(len(option)):
                     temp = temp.replace(unknowns[i], str(option[i]))
                 try:
                     x = eval(temp)
                     if (not x < 0.1) or (not x > -0.1):
+                        wrongSolution += 1
                         bad = True
                 except:
                     unsure += 1
-
+    if newEntry['question'] == "":
+        emptyQuestion += 1
+        bad = True
     if bad:
         count += 1
     else:
@@ -89,8 +111,15 @@ for entry in raw3:
     }
 
     newData.append(newEntry)
-
-print(count, mcq, unsure, len(newData), len(raw1+raw2+raw3))
+print("Original Size: ", len(raw1+raw2+raw3))
+print("Size of New Dataset: ", len(newData))
+print("Bad Examples: ", count)
+print("  Non Numerical: ", nonNum)
+print("    out of which mcq: ", mcq)
+print("  Empty Unkowns: ", emptyUnknowns)
+print("  Empty Question: ", emptyQuestion)
+print("  Bad Solution: ", wrongSolution)
+print("Unsure of Correctness of Solution: ", unsure)
 
 fpout = open('../data/combined.json', 'w')
 json.dump(newData, fpout, indent=2)
